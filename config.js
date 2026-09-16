e250f6c275ce65a4e71a87eb089d1b6cf4cc4b24
salmardeliaa <salmardelia3@gmail.com>
2026-09-16 13:45:38 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://jkrprhjpmujajinjxhwb.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_jEij8tLtvyQyQkKarIPkVA_3lJbBKEn";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
