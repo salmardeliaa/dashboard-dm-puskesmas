6f3c647e87b63ea1c7c475254fc1998ae27acc19
salmardeliaa <salmardelia3@gmail.com>
2026-09-16 13:40:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://jkrprhjpmujajinjxhwb.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_jEij8tLtvyQyQkKarIPkVA_3lJbBKEn";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
